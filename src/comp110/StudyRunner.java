9f8c2ca4b46e51a435ed5bfda9c7da0e7afeb4b7
Kris Jordan <krisjordan@gmail.com>
2016-10-31 10:12:39 -0400

    Update test descriptions in StudyRunner

--- src/comp110/StudyRunner.java
+++ src/comp110/StudyRunner.java
@@ -6,9 +6,9 @@ public class StudyRunner {
 
     // TODO: Test Part 2.1 and 2.2
 
-    // TODO: Test Part 2.3 and 2.4
+    // TODO: Test Part 2.3
 
-    // TODO: Test Part 2.5
+    // TODO: Test Part 2.4 and 2.5
 
   }
 
@@ -17,49 +17,49 @@ public class StudyRunner {
    * them by selecting all of the commented lines, right clicking, finding the
    * "Source" menu and selecting "Toggle Comment."
    */
-  //  public static Sample generateSampleA() {
-  //    Sample sample = new Sample("Baby Doe");
-  //    sample.addAllele("AA");
-  //    sample.addAllele("AA");
-  //    sample.addAllele("AA");
-  //    sample.addAllele("AT");
-  //    sample.addAllele("AT");
-  //    sample.addAllele("TA");
-  //    sample.addAllele("TC");
-  //    sample.addAllele("TG");
-  //    sample.addAllele("GT");
-  //    sample.addAllele("TT");
-  //    return sample;
-  //  }
+  // public static Sample generateSampleA() {
+  // Sample sample = new Sample("Baby Doe");
+  // sample.addAllele("AA");
+  // sample.addAllele("AA");
+  // sample.addAllele("AA");
+  // sample.addAllele("AT");
+  // sample.addAllele("AT");
+  // sample.addAllele("TA");
+  // sample.addAllele("TC");
+  // sample.addAllele("TG");
+  // sample.addAllele("GT");
+  // sample.addAllele("TT");
+  // return sample;
+  // }
   //
-  //  public static Sample generateSampleB() {
-  //    Sample sample = new Sample("John Doe");
-  //    sample.addAllele("GG");
-  //    sample.addAllele("GG");
-  //    sample.addAllele("GG");
-  //    sample.addAllele("AT");
-  //    sample.addAllele("AT");
-  //    sample.addAllele("TA");
-  //    sample.addAllele("TC");
-  //    sample.addAllele("TG");
-  //    sample.addAllele("GT");
-  //    sample.addAllele("TT");
-  //    return sample;
-  //  }
+  // public static Sample generateSampleB() {
+  // Sample sample = new Sample("John Doe");
+  // sample.addAllele("GG");
+  // sample.addAllele("GG");
+  // sample.addAllele("GG");
+  // sample.addAllele("AT");
+  // sample.addAllele("AT");
+  // sample.addAllele("TA");
+  // sample.addAllele("TC");
+  // sample.addAllele("TG");
+  // sample.addAllele("GT");
+  // sample.addAllele("TT");
+  // return sample;
+  // }
   //
-  //  public static Sample generateSampleC() {
-  //    Sample sample = new Sample("John Smith");
-  //    sample.addAllele("GG");
-  //    sample.addAllele("GG");
-  //    sample.addAllele("GG");
-  //    sample.addAllele("AT");
-  //    sample.addAllele("AG");
-  //    sample.addAllele("TA");
-  //    sample.addAllele("TC");
-  //    sample.addAllele("TG");
-  //    sample.addAllele("GT");
-  //    sample.addAllele("TT");
-  //    return sample;
-  //  }
+  // public static Sample generateSampleC() {
+  // Sample sample = new Sample("John Smith");
+  // sample.addAllele("GG");
+  // sample.addAllele("GG");
+  // sample.addAllele("GG");
+  // sample.addAllele("AT");
+  // sample.addAllele("AG");
+  // sample.addAllele("TA");
+  // sample.addAllele("TC");
+  // sample.addAllele("TG");
+  // sample.addAllele("GT");
+  // sample.addAllele("TT");
+  // return sample;
+  // }
 
 }
